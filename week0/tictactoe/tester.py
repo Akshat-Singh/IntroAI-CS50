e77072de9b12c955df11a70775e5fcc1f96ea214
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-05-23 12:54:35 +0530

    Changes to the tictactoe file

---
+++ week0/tictactoe/tester.py
@@ -0,0 +1,4 @@
+from tictactoe import *
+
+print(f"{player([[EMPTY, EMPTY, X], [O, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]])}")
+print(f"{winner([[X, O, O], [X, X, EMPTY], [O, O, X]])}")
--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import deepcopy
 
 X = "X"
 O = "O"
@@ -11,7 +12,7 @@ EMPTY = None
 
 def initial_state():
     """
-    Returns starting state of the board.
+    [COMPLETED]Returns starting state of the board.
     """
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
@@ -20,48 +21,87 @@ def initial_state():
 
 def player(board):
     """
-    Returns player who has the next turn on a board.
+    [COMPLETED]Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    emptyCells = 0 
+    for row in board: 
+        for element in row: 
+            emptyCells = emptyCells + 1 if element is EMPTY else emptyCells
+
+    return X if emptyCells % 2 == 1 else O
 
 
 def actions(board):
     """
-    Returns set of all possible actions (i, j) available on the board.
+    [COMPLETED]Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possibleMoves = set()
+    for i in range(len(board)): 
+        for j in range(len(board[0])): 
+            if board[i][j] is EMPTY: 
+                possibleMoves.add((i, j))
+    
+    return possibleMoves
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    newBoardState = deepcopy(board)
+    if newBoardState[action[0]][action[1]] is not EMPTY: 
+        raise Exception('Block already filled')
+    
+    newBoardState[action[0]][action[1]] = player(newBoardState)
+    return newBoardState 
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range (len(board)):
+        if board[1][1] == board[2][2] == board[0][0]:
+            return board[1][1]
+        elif board[2][0] == board[1][1] == board[0][2]:
+            return board[1][1]
+        elif board[i][0] == board[i][1] == board[i][2]:
+            return board[i][1]
+        elif board[0][i] == board[1][i] == board[2][i]:
+            return board[1][i]
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) is not None: 
+        return True
+    
+    for row in board: 
+        for element in row: 
+            if element is EMPTY: 
+                return False
+    
+    return True
+    
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) is X: 
+        return 1
+    elif winner(board) is O: 
+        return -1 
+    elif winner(board) is None: 
+        return 0 
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    
